bd73c27c2fa4b45995f834857eb5267714d835c9
Elias Grafström <elias@supershow.se>
2024-09-30 08:52:47 +0200

    Working on list command.

---
+++ ProductCatalog/ProductCatalog/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ProductCatalog
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Price  { get; set; }
+
+        public Product(string inputName, double inputPrice)
+        {
+            this.Name = inputName;
+            this.Price = inputPrice;
+        }
+
+    }
+}
---
+++ ProductCatalog/ProductCatalog/Program.cs
@@ -0,0 +1,38 @@
+﻿using ProductCatalog;
+using System.Runtime.InteropServices;
+
+
+var MyProductList = new List<Product>();
+
+Console.WriteLine("What do you wanna do? exit, add, list");
+var Answer = Console.ReadLine();
+
+while (Answer == "add")
+{
+    Console.WriteLine("Enter Product Name");
+    var ProductName = Console.ReadLine();
+    Console.WriteLine("How Much?");
+    var ProductPrice = Console.ReadLine();
+
+    var PruductPriceDouble = double.Parse(ProductPrice);
+    var MyObject = new Product(ProductName, PruductPriceDouble);
+    
+    MyProductList.Add(MyObject);
+
+    foreach (var CurrentProduct in MyProductList)
+    {
+        Console.WriteLine(CurrentProduct.Name + " " + CurrentProduct.Price + "£");
+    }
+    Console.WriteLine("What do you wanna do? exit, add");
+    Answer = Console.ReadLine();
+
+    if (Answer == "list")
+    {
+        Console.WriteLine(MyObject);
+    }
+}
+
+
+Console.ReadKey();
+
+
